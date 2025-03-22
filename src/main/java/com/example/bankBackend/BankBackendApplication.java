b09258b4288f35a8f30511c8cf7bc8d487822653
dmytro_demchuk <dimon.pupkin.96@gmail.com>
2025-03-22 20:19:41 +0200

    Minor changes

--- src/main/java/com/example/bankBackend/BankBackendApplication.java
+++ src/main/java/com/example/bankBackend/BankBackendApplication.java
@@ -26,13 +26,18 @@ public class BankBackendApplication {
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList(
 				"http://localhost:4200",
-				"https://charming-duckanoo-6a38b9.netlify.app"
+				"https://comfy-mooncake-d66bce.netlify.app",
+				"https://comfy-mooncake-d66bce.netlify.app/"
 		));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+		corsConfiguration.setAllowedHeaders(Arrays.asList(
+				"Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"
+		));
+		corsConfiguration.setExposedHeaders(Arrays.asList(
+				"Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"
+		));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
